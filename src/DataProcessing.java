017145fc4f1418610a197f54d379e190fc1cefd5
E343C <e.cesari@studenti.unibs.it>
2021-05-23 16:30:27 +0200

    Sistemato un paio di cose

--- src/DataProcessing.java
+++ src/DataProcessing.java
@@ -2,23 +2,34 @@ import java.util.ArrayList;
 
 public class DataProcessing
 {
-	private static ArrayList <City> cities = new ArrayList<>();
+	private static ArrayList<City> cities = new ArrayList<>();
 	
 	public static ArrayList<City> getCities()
 	{
 		return cities;
 	}
+	
+	public static void out ()
+	{
+		for (City c:cities)
+		{
+			c.stampaCity();
+		}
+	}
 
-	public static City searchCity(int idCercato)
+/*	public static City searchCity(int idCercato)
 	{
 		int left=0, right=cities.size()-1;
 		int middle;
-		while(left <= right) {
+		while(left <= right)
+		{
 			middle = (left+right)/2;
 			int id_temp = cities.get(middle).getId();
-			if (id_temp < idCercato){
+			if (id_temp < idCercato)
+			{
 				left=middle+1;
-			}else if (id_temp > idCercato)
+			}
+			else if (id_temp > idCercato)
 			{
 				right=middle-1;
 			}
@@ -28,5 +39,5 @@ public class DataProcessing
 			}
 		}
 		return null;
-	}
+	}*/
 }
